e938439281a87c4dc824cc33b3a2079a61c016cd
gonzaloiv <aelthalion@gmail.com>
2017-04-26 17:31:41 +0900

    Limpieza, recomienzo de proyecto

--- Assets/Scenes/Opening/TitleController.cs
+++ Assets/Scenes/Opening/TitleController.cs
@@ -3,22 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using Prime31.TransitionKit;
 
 public class TitleController : MonoBehaviour, IPointerClickHandler {
 
-  #region IPointerClickHandler implementation
+  #region IPointerClickHandler
 
   public void OnPointerClick(PointerEventData pointerEventData) {    
-
-//    var fader = new FadeTransition() {
-//      nextScene = 1,
-//      fadeToColor = Color.white
-//    };
-//    TransitionKit.instance.transitionWithDelegate(fader);
-
     SceneManager.LoadScene(1);
-
   }
 
   #endregion
